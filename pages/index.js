43c22831080f47b108e10e318300eab13e4fe457
gilveloso1 <gilveloso.oficial@gmail.com>
2025-10-23 15:13:01 +0000

    modifica mensagem e cria botões malucos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
+
+
 function Home() {
-    return <h1>Olá, mundo!</h1>
+    const [troca, setTroca] = useState(true)
+    function trocar(){
+        if (troca) setTroca(false);
+        if (!troca) setTroca(true);
+    }
+    return (
+        <div>
+            <h1>:) Você me ama? ❤️</h1>
+            <button onClick={trocar} style={{width: 200, height: 30}}>
+                {troca ? 'Sim' : 'Não'}
+            </button>
+            <div style={{paddingBottom: 10}}></div>
+            <button onClick={trocar} style={{width: 200, height: 30}}>
+                {troca ? 'Não' : 'Sim'}
+            </button>
+            <h2>{troca ? '' : 'NÃO AMO!!!! 😑'}</h2>
+        </div>
+    )
 }
 
 export default Home;
