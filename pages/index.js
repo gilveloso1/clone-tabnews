5edb9b97b23f71d9334dec92b87d09b8e99b92a6
gilveloso1 <gilveloso.oficial@gmail.com>
2025-10-24 01:25:42 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,30 @@
-import { useState } from 'react';
-
-
+import { useState } from "react";
 
 function Home() {
-    const [troca, setTroca] = useState(true)
-    function trocar(){
-        if (troca) setTroca(false);
-        if (!troca) setTroca(true);
-    }
-    return (
-        <div>
-            <h1>:) Você me ama? ❤️</h1>
-            <button onClick={trocar} style={{width: '200px', height: '30px', fontSize: '18px'}}>
-                {troca ? 'Sim' : 'Não'}
-            </button>
-            <div style={{paddingBottom: '10px'}}></div>
-            <button onClick={trocar} style={{width: '200px', height: '30', fontSize: '18px'}}>
-                {troca ? 'Não' : 'Sim'}
-            </button>
-            <h2>{troca ? '' : 'NÃO AMO!!!! 😑'}</h2>
-        </div>
-    )
+  const [troca, setTroca] = useState(true);
+  function trocar() {
+    if (troca) setTroca(false);
+    if (!troca) setTroca(true);
+  }
+  return (
+    <div>
+      <h1>:) Você me ama? ❤️</h1>
+      <button
+        onClick={trocar}
+        style={{ width: "200px", height: "30px", fontSize: "18px" }}
+      >
+        {troca ? "Sim" : "Não"}
+      </button>
+      <div style={{ paddingBottom: "10px" }}></div>
+      <button
+        onClick={trocar}
+        style={{ width: "200px", height: "30", fontSize: "18px" }}
+      >
+        {troca ? "Não" : "Sim"}
+      </button>
+      <h2>{troca ? "" : "NÃO AMO!!!! 😑"}</h2>
+    </div>
+  );
 }
 
 export default Home;
